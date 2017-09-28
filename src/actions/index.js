110ad27a4af607e88860af38deaaaa3337ec6d44
brentkirkland <brentckirkland@gmail.com>
2017-09-27 21:35:37 -0700

    added ignore

--- src/actions/index.js
+++
@@ -1,9 +0,0 @@
-
-export const instantiateWeb3 = () => ({
-  type: 'INSTANTIATE_WEB3'
-});
-
-export const addColorZero = (arr) => ({
-  type: 'ADD_COLOR_ZERO',
-  arr: arr
-})
--- truffle_script.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-rm -rf build/contracts
-rm -rf src/contracts
-truffle compile
-truffle migrate
-rm -rf src/contracts
-mkdir src/contracts
-cp build/contracts/* src/contracts
